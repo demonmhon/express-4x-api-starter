1c136abcc66d2df2b103cca44ce66ab3128a4480
demonmhon <thanawat.petchsri@gmail.com>
2022-03-12 00:48:25 +0700

    Update packages, simplify code

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,21 +1,22 @@
-/* eslint-disable no-console */
-/* eslint-disable no-underscore-dangle */
 const { createLogger, format, transports } = require('winston');
-const { logLevel } = require('../config');
+
+const { env, logLevel } = require('../config');
 
 const logger = createLogger({
   format: format.json(),
-  transports: [
-    new transports.Console({
-      level: logLevel,
-      format: format.combine(
-        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        format.colorize(),
-        format.simple()
-      ),
-      handleExceptions: true,
-    }),
-  ],
+  transports: !['test'].includes(env)
+    ? [
+        new transports.Console({
+          level: logLevel,
+          format: format.combine(
+            format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+            format.colorize(),
+            format.simple()
+          ),
+          handleExceptions: true,
+        }),
+      ]
+    : [],
 });
 
 module.exports = logger;
